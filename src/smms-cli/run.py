1da818e933806c288be53cd57d08c6ec726b7092
cascades-sjtu <cascades@sjtu.edu.cn>
2022-03-28 14:55:45 +0800

    merge to utils.py and create run.py

--- src/smms-cli/run.py
+++ src/smms-cli/run.py
@@ -1,4 +1,41 @@
 import argparse
+import utils
+
+
+class Client:
+    def __init__(self):
+        try:
+            from apitoken import APIToken
+
+            self.APIToken = APIToken
+        except ImportError:
+            import getpass
+
+            print(
+                "sm.ms-cli needs username and password to get APIToken(for first time use)"
+            )
+            username = input("Please input your username: ")
+            password = getpass.getpass("Please input your password: ")
+            response = utils.getAPIToken(username, password)
+            self.APIToken = response["data"]["token"]
+            self.betterPrint(response)
+
+    def betterPrint(self, json_data):
+        from pygments import highlight, lexers, formatters
+        import json
+
+        json_raw = json.dumps(json_data, indent=4)
+        print(highlight(json_raw, lexers.JsonLexer(), formatters.TerminalFormatter()))
+
+    def uploadImage(self, filename: str):
+        response = utils.uploadImage(filename, self.APIToken)
+        self.betterPrint(response)
+
+    def getUserProfile(self):
+        response = utils.getUserProfile(self.APIToken)
+        self.betterPrint(response)
+
 
 if __name__ == "__main__":
-    pass
+    c = Client()
+    c.getUserProfile()
--- src/smms-cli/utils.py
+++ src/smms-cli/utils.py
@@ -1,7 +1,35 @@
 import requests
-import json
 
 endpointBase = "https://sm.ms/api/v2/"
+
+
+def getAPIToken(username: str, password: str) -> list:
+    endpoint = endpointBase + "token"
+    params = {
+        "username": username,
+        "password": password,
+    }
+    r = requests.post(url=endpoint, params=params)
+
+    # write token to one file for module test
+    import sys
+
+    tokenfile = sys.path[0] + "/apitoken.py"
+    response = r.json()
+    with open(tokenfile, "w") as f:
+        content = '{} = "{}"'.format("APIToken", response["data"]["token"])
+        f.write(content + "\n")
+    return response
+
+
+def getUserProfile(APIToken: str, ContentType: str = "multipart/form-data") -> list:
+    endpoint = endpointBase + "profile"
+    headers = {
+        "Content-Type": ContentType,
+        "Authorization": APIToken,
+    }
+    r = requests.post(url=endpoint, headers=headers)
+    return r.json()
 
 
 def getUploadHistoryByIP(APIToken: str, format: str = "json") -> list:
@@ -33,6 +61,7 @@ def deleteImage(hash: str, format: str = "json") -> list:
 
 def getUploadHistory(APIToken: str, ContentType: str = "multipart/form-data") -> list:
     import itertools
+    import json
 
     history = json.loads("[]")
     endpoint = endpointBase + "upload_history"
@@ -67,12 +96,15 @@ def uploadImage(
 
 
 def test():
+    import json
     from apitoken import APIToken
 
-    # print(json.dumps(getUploadHistoryByIP(), indent=4))
-    # print(json.dumps(clearUploadHistoryByIP(), indent=4))
-    # print(json.dumps(deleteImage("AdRvXkFcSLiDyHh41ZI2zpP8UM"), indent=4))
-    # print(json.dumps(getUploadHistory(), indent=4))
+    print(json.dumps(getAPIToken("cascades", "Chen112203"), indent=4))
+    print(json.dumps(getUserProfile(APIToken), indent=4))
+    print(json.dumps(getUploadHistoryByIP(), indent=4))
+    print(json.dumps(clearUploadHistoryByIP(), indent=4))
+    print(json.dumps(deleteImage("AdRvXkFcSLiDyHh41ZI2zpP8UM"), indent=4))
+    print(json.dumps(getUploadHistory(), indent=4))
     print(json.dumps(uploadImage("tests/avatar.jpeg", APIToken), indent=4))
 
 
